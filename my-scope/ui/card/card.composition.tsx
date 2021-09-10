0ea9daae7c3d21b6009005ace62348d76ba5cab8
tmaioli <tmaioli@deloitte.it>
2021-09-10 08:21:16 +0100

    small integrations to card component

--- my-scope/ui/card/card.composition.tsx
+++ my-scope/ui/card/card.composition.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { Card } from './card';
 
 export const BasicCard = () => (
-  <div style={{width: "500px"}}>
-  <Card text="hello from Card" />
-  </div>
+  <Card 
+        title='a generic card without image'
+        text='this some text styled as paragraph'
+        />
+);
+export const cardWithImg = () => (
+  <Card 
+        imgSrc='https://randomwordgenerator.com/img/picture-generator/54e1d3424d52ad14f1dc8460962e33791c3ad6e04e507440722d72d59144cd_640.jpg'
+        title='a generic card'
+        text='this some text styled as paragraph'
+        />
 );
